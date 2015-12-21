8cb5a9b542171c701e764e332ec60fc6fa5aa5b9
cm710 <cm710@cam.ac.uk>
2015-12-21 20:46:05 +0200

    added skeleton for the front page and the button page

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+</head>
+
+<title>Bomb helper</title>
+<body>
+<h1>Welcome to the bomb helper website</h1>
+<h2>Click below for a puzzle you might need help with</h2>
+
+<ul>
+  <li><a href=./button.html>Button</a></li>
+  
+</ul>
+
+</body>
+</html>
